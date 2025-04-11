3b6f2448eb9a93c1a610ffb9d42d800b1fdb078d
vitorbmulford <vitorbmulford@vitor-dev.vitor-dev>
2025-04-11 09:28:51 -0300

    criando home layout

---
+++ components/BookList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookList = () => {
+  return (
+    <section>
+        <h2 className='font-bebas-neue text-4xl text-light-100'>Popular BOoks</h2>
+    </section>
+  )
+}
+
+export default BookList
---
+++ components/BookOverview.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookOverview = () => {
+  return (
+    <section className='book-overview'>
+      <div className='flex flex-1 flex-col gap5'>
+        <h1>A long book Title</h1>
+      </div>
+    </section>
+  )
+}
+
+export default BookOverview
